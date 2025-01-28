0f9ef7691223c5112bd71d4c4e25aa82cdc3c8a3
Carlos Cabral <carloscabral@google.com>
2025-01-28 16:11:17 -0300

    vai

--- camara-oidc-ciba/deploy-ciba.sh
+++ camara-oidc-ciba/deploy-ciba.sh
@@ -15,6 +15,10 @@
 # limitations under the License.
 
 
+# ==============================================================================
+# Pre-Setup: Environment Checks and Tool Installation
+# ==============================================================================
+
 # Check for required environment variables
 check_env_var() {
   var_name="$1"
@@ -32,15 +36,18 @@ check_env_var CIBA_TARGET_SERVER_URI
 check_env_var PRIVATE_KEY
 check_env_var USE_MOCK
 
-
-
-echo "Installing apigeecli"
+echo "Installing apigeecli..."
 curl -s https://raw.githubusercontent.com/apigee/apigeecli/main/downloadLatest.sh | bash
-export PATH=$PATH:$HOME/.apigeecli/bin
+export PATH="$PATH:$HOME/.apigeecli/bin"
 
 # Get Google Cloud access token
-TOKEN=$(gcloud auth print-access-token)
-gcloud config set project "$APIGEE_PROJECT_ID"
+TOKEN=$(gcloud auth print-access-token) || { echo "Error: Could not get Google Cloud access token."; exit 1; }
+gcloud config set project "$APIGEE_PROJECT_ID" || { echo "Error: Could not set Google Cloud project."; exit 1; }
+
+
+# ==============================================================================
+# Placeholder Updates
+# ==============================================================================
 
 echo "Updating placeholders..."
 
@@ -57,45 +64,49 @@ redirect_uri=https://localhost
 jwks_uri=$CLIENT_JKWS_URI"
 echo "$PRE_PROP" > ./apiproxy/resources/properties/ciba.properties || { echo "Error: Could not update ciba.properties"; exit 1; }
 
-sed -e 's/#PATH_PLACEHOLDER#/'"${CIBA_TARGET_SERVER_PATH}"'/g' ./apiproxy/targets/default_tmpl.xml > ./apiproxy/targets/default.xml || { echo "Error: Could not update default.xml"; exit 1; }
+sed -e 's/#PATH_PLACEHOLDER#/"${CIBA_TARGET_SERVER_PATH}"/g' ./apiproxy/targets/default_tmpl.xml > ./apiproxy/targets/default.xml; rm ./apiproxy/targets/default_tmpl.xml || { echo "Error: Could not update default.xml"; exit 1; }
 
 echo "Placeholders updated successfully."
+
+
+# ==============================================================================
+# Config Setup: Target Servers and KVMs
+# ==============================================================================
 
 echo "Creating necessary configs..."
 
 echo "Creating CIBA target server..."
-
-apigeecli targetservers create --name camara-oidc-ciba-backend  --org "$APIGEE_PROJECT_ID" --env "$APIGEE_ENV" --tls true --port 443 --host "$CIBA_TARGET_SERVER_URI" --token "$TOKEN" \
-|| { echo "Error: Could not create target server or it already exists. Proceeding with the setup..."; }
-
+apigeecli targetservers create --name camara-oidc-ciba-backend  --org "$APIGEE_PROJECT_ID" --env "$APIGEE_ENV" --tls true --port 443 --host "$CIBA_TARGET_SERVER_URI" --token "$TOKEN" || { echo "Error: Could not create target server or it already exists. Proceeding with the setup..."; }
 echo "Creation of Target Server done"
 
-
 echo "Creating env-scoped KVM and KVM Entry for Private Key..."
-apigeecli kvms create --name camara-oidc-ciba  --org "$APIGEE_PROJECT_ID" --env "$APIGEE_ENV"  --token "$TOKEN" \
-|| { echo "Error: Could not create KVM or it already exists. Proceeding with the setup...";  }
+apigeecli kvms create --name camara-oidc-ciba  --org "$APIGEE_PROJECT_ID" --env "$APIGEE_ENV"  --token "$TOKEN" || { echo "Error: Could not create KVM or it already exists. Proceeding with the setup..."; }
 
-apigeecli kvms entries create -m camara-oidc-ciba -k "id_token_private_key" --value "${PRIVATE_KEY}" --org "$APIGEE_PROJECT_ID" --env "$APIGEE_ENV"  --token "$TOKEN" \
-|| { echo "Error: Could not create KVM entry. Exiting..."; exit 1; }
+apigeecli kvms entries create -m camara-oidc-ciba -k "id_token_private_key" --value "${PRIVATE_KEY}" --org "$APIGEE_PROJECT_ID" --env "$APIGEE_ENV"  --token "$TOKEN" && echo "KVM Entry created successfully" || ( \
+  ret=$?
+  if [[ $ret -eq 409 ]]; then
+    echo "Warning: KVM Entry 'id_token_private_key' already exists.  Continuing..."
+  else
+    echo "Error: Could not create KVM entry. Error code: $ret. Exiting..."
+    exit 1
+  fi
+)
+
+
+# ==============================================================================
+# Apigee Proxy Setup and Deployment
+# ==============================================================================
 
 echo "Deploying Apigee artifacts..."
 
 echo "Importing and Deploying Apigee camara-oidc-ciba-v1 proxy..."
-
-REV=$(apigeecli apis create bundle -f ./apiproxy -n camara-oidc-ciba-v1 --org "$APIGEE_PROJECT_ID" --token "$TOKEN" --disable-check | jq ."revision" -r) \
-|| { echo "Error: Could not create Apigee proxy bundle. Output: $REV"; exit 1; }
-
-apigeecli apis deploy --wait --name camara-oidc-ciba-v1 --ovr --rev "$REV" --org "$APIGEE_PROJECT_ID" --env "$APIGEE_ENV" --token "$TOKEN" \
-|| { echo "Error: Could not deploy Apigee proxy."; exit 1; }
+REV=$(apigeecli apis create bundle -f ./apiproxy -n camara-oidc-ciba-v1 --org "$APIGEE_PROJECT_ID" --token "$TOKEN" --disable-check | jq ."revision" -r) || { echo "Error: Could not create Apigee proxy bundle. Output: $REV"; exit 1; }
+apigeecli apis deploy --wait --name camara-oidc-ciba-v1 --ovr --rev "$REV" --org "$APIGEE_PROJECT_ID" --env "$APIGEE_ENV" --token "$TOKEN" || { echo "Error: Could not deploy Apigee proxy."; exit 1; }
 
 if [[ "$USE_MOCK" == "true" ]]; then
   echo "Using mock backend. Deploying mock proxy..."
-
-  REV=$(apigeecli apis create bundle -f ./ciba-mock-backend/apiproxy -n camara-oidc-ciba-mock-backend-v1 --org "$APIGEE_PROJECT_ID" --token "$TOKEN" --disable-check | jq ."revision" -r) \
-  || { echo "Error: Could not create Apigee proxy bundle. Output: $REV"; exit 1; }
-
-  apigeecli apis deploy --wait --name camara-oidc-ciba-mock-backend-v1 --ovr --rev "$REV" --org "$APIGEE_PROJECT_ID" --env "$APIGEE_ENV" --token "$TOKEN" \
-  || { echo "Error: Could not deploy Apigee proxy."; exit 1; }
+  REV=$(apigeecli apis create bundle -f ./ciba-mock-backend/apiproxy -n camara-oidc-ciba-mock-backend-v1 --org "$APIGEE_PROJECT_ID" --token "$TOKEN" --disable-check | jq ."revision" -r) || { echo "Error: Could not create Apigee mock proxy bundle. Output: $REV"; exit 1; }
+  apigeecli apis deploy --wait --name camara-oidc-ciba-mock-backend-v1 --ovr --rev "$REV" --org "$APIGEE_PROJECT_ID" --env "$APIGEE_ENV" --token "$TOKEN" || { echo "Error: Could not deploy Apigee mock proxy."; exit 1; }
 fi
 
 echo "Deployment successful!"
