a1188b72b8f17193f4fcf25ea08c33e67d544a90
Carlos Cabral <carloscabral@google.com>
2025-01-31 19:02:03 -0300

    vai

--- sim-swap/mock-sim-swap/apiproxy/resources/jsc/validate-msisdn-v1.js
+++
@@ -1,5 +0,0 @@
-var msisdn = context.getVariable("msisdn");
-//var status = ["447772000001", "447772000002", "447772000004", "447772000006"].includes(msisdn);
-var status = ["+34678668000"].includes(msisdn);
-
-context.setVariable("status", status);
